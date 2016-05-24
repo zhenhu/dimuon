de7aab412b63c1fc8729e315a98ca4411d51ddff
Zhen <zhen.hu@cern.ch>
2016-05-24 13:29:11 -0500

    update for 2016 data

--- test/crabJobs/crab3_DoubleMuonLowMass.py
+++ test/crabJobs/crab3_DoubleMuonLowMass.py
@@ -1,34 +1,20 @@
-from WMCore.Configuration import Configuration
-config = Configuration()
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
 
-config.section_('General')
+config.General.requestName = 'DoubleMuonLowMass2016Bv1'
 config.General.transferOutputs = True
-#config.General.requestName = 'DoubleMuonLowMass2015Cv1'
-config.General.requestName = 'DoubleMuonLowMass2015Dv4'
+config.General.transferLogs = True
 
-config.section_('JobType')
-config.JobType.psetName = '../runOnia2MuMuRootupler.py'
 config.JobType.pluginName = 'Analysis'
-#config.JobType.outputFiles = ['Onia2MuMuPAT.root']
+config.JobType.psetName = '../runOnia2MuMuRootupler.py'
+#config.JobType.outputFiles = ['Rootuple_test.root']
 
-config.section_('Data')
-#config.Data.inputDataset = '/DoubleMuonLowMass/zhenhu-Run2015C-Onia2MuMuPAT-v1-ca8022810ea1b04e8e12c6056abc0a7d/USER'
-config.Data.inputDataset = '/DoubleMuonLowMass/zhenhu-Run2015D-Onia2MuMuPAT-v4-ca8022810ea1b04e8e12c6056abc0a7d/USER'
+config.Data.inputDataset = '/DoubleMuonLowMass/asanchez-BPHSkim-v1-Run2016B-PromptReco-v2-f3371d8706d2a64e31e991df87f57a46/USER'
 config.Data.inputDBS = 'phys03'
-config.Data.unitsPerJob = 50
-config.Data.splitting = 'LumiBased'
-#config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
-#config.Data.outLFNDirBase = '/store/user/yik/myfourmuonNtpl/muonia/2015Dwith25nsv3'
-#config.Data.lumiMask = 'Cert_246908-258159_13TeV_PromptReco_Collisions15_25ns_JSON_MuonPhys.txt'
-config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_MuonPhys.txt'
-#config.Data.runRange = '193093-193999' # '193093-194075'
-config.Data.publication = True
-#config.Data.outputDatasetTag = 'Run2015C-Onia2MuMuRootuple-v1'
-config.Data.outputDatasetTag = 'Run2015D-Onia2MuMuRootuple-v4'
-#config.Data.ignoreLocality = True
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 10
 
-config.section_('User')
-
-config.section_('Site')
-config.Site.storageSite = 'T3_US_FNALLPC' 
-
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+config.Data.outputDatasetTag  = 'Run2016B-Onia2MuMuRootuple-v1'
+config.Site.storageSite = 'T3_US_FNALLPC'
